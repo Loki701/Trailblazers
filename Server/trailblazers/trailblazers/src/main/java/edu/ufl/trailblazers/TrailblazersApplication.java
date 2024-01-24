4214b9f0a634e94d01cd0ce4aeeef6ade4f0aadb
dfserrano1 <dfserra01@yahoo.com>
2024-01-23 19:42:07 -0500

    Spring Boot Back End

---
+++ Server/trailblazers/trailblazers/src/main/java/edu/ufl/trailblazers/TrailblazersApplication.java
@@ -0,0 +1,13 @@
+package edu.ufl.trailblazers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailblazersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrailblazersApplication.class, args);
+	}
+
+}
---
+++ Server/trailblazers/trailblazers/src/test/java/edu/ufl/trailblazers/TrailblazersApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ufl.trailblazers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrailblazersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
