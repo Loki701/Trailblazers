70646ec63a45928e1551a21d6ff014202ca4daa6
dfserrano1 <dfserra01@yahoo.com>
2024-01-25 20:08:57 -0500

    Set up Controller and Service folders

--- Server/trailblazers/trailblazers/src/main/java/edu/ufl/trailblazers/Controller/TrailblazersAppController.java
+++ Server/trailblazers/trailblazers/src/main/java/edu/ufl/trailblazers/Controller/TrailblazersAppController.java
@@ -1,4 +1,4 @@
-package edu.ufl.trailblazers;
+package edu.ufl.trailblazers.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
