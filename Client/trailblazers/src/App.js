2f3f6841beba78a0ad7b5fe97a891f310e4d750a
Loki701 <figueredoj189@gmail.com>
2024-01-30 19:14:41 -0500

    update

--- Client/trailblazers/src/App.js
+++ Client/trailblazers/src/App.js
@@ -1,6 +1,6 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Nav  from './components/Nav';
 import Home from "./components/Home";
 import About from './components/About';
