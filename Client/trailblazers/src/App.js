2289abf977b04824c5d963ecf23bf7c8c31c2905
Loki701 <figueredoj189@gmail.com>
2024-03-09 03:01:41 -0500

    Redesign landing page
    added:
    api call
    full maze functionality
    complete redesign

--- Client/trailblazers/src/App.js
+++ Client/trailblazers/src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
+import {
+  Route,
+   Routes, BrowserRouter
+} from "react-router-dom";
+
+// import { Algorithm, Community, MiniGames, Resources, Navbar, AboutUs, Home } from './components';
+
+import LandingPage from './components/header/landingPage/LandingPage';
+import Header from './components/header/Header';
 
 function App() {
   return (
-    <div className="App">
-      
+    <BrowserRouter>
+    <div className="app">
+
+      <Header />
+    
+      <Routes>
+        <Route path="/" element={<LandingPage/>}/>
+        {/* <Route path="/algorithms" element={<Algorithm/>}/> */}
+        {/* <Route path="/mini-games" element={<MiniGames/>}/> */}
+        {/* <Route path="/community" element={<Community/>}/> */}
+        {/* <Route path="/resources" element={<Resources/>}/> */}
+        {/* <Route path="/about-us" element={<AboutUs/>}/> */}
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ Client/trailblazers/src/components/header/Header.jsx
@@ -0,0 +1,70 @@
+import "./Header.css";
+import { useState } from "react";
+import Logo from "../../images/logo.png";
+import { useNavigate } from "react-router-dom";
+
+function className(...classes) {
+    return classes.filter(Boolean).join(" ");
+  }
+
+
+const Header  = () => {
+    const [menuActive, setMenuActive] = useState(false);
+    const navigate = useNavigate();
+
+    const handleExploreClick = (path) => {
+        setMenuActive(false);
+        navigate(path);
+    }
+
+
+    const handleMobileMenuClick = () => {
+        setMenuActive(!menuActive);
+    } 
+    return(
+        <header id="header" className="header">
+            <div className="header-container">
+                <div className="content">
+                    <img src={Logo} alt="Logo" onClick={() => handleExploreClick("/")}/>
+                </div>
+
+                {menuActive? (
+                    <span class="material-symbols-outlined" onClick={handleMobileMenuClick}>
+                    close
+                    </span>
+                ):(    
+                <span class="material-symbols-outlined" onClick={handleMobileMenuClick}>
+                menu
+                </span>
+                )}
+
+                <div className = {className(menuActive? "active": "", "navbar")} >
+                    <ul className="column">
+                        <li className="link">
+                            <a onClick={() => handleExploreClick("/algorithms")}>Explore Algorithms</a>
+                        </li>
+                        <li className="link">
+                            <a onClick={() => handleExploreClick("/mini-games")}>MiniGames</a>
+                        </li>
+                        <li className="link">
+                            <a onClick={() => handleExploreClick("/about-us")}>About Us</a>
+                        </li>
+                        <li className="link">
+                            <a onClick={()=> handleExploreClick("/community")}>Community</a>
+                        </li>
+                        <li className="link">
+                            <a onClick={()=> handleExploreClick("/resources")}>Resources</a>
+                        </li>
+                    </ul>
+                    {/* <div className="actions">
+                        <button className="button">Sing Up</button>
+                        <button className="button">Log In</button>
+                    </div> */}
+                </div>
+                
+            </div>
+        </header>
+    );
+}
+
+export default Header;
